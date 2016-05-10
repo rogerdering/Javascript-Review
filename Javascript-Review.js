4dbe6355a493a33af3cf814a4caefc728099c70a
Roger Dering <rogerdering@hotmail.com>
2016-05-10 13:51:09 +0200

    fixed

--- Javascript-Review.js
+++ Javascript-Review.js
@@ -5,9 +5,9 @@ function fruits (apple, grape, lemon) {
 console.log(fruits(1, 3, 5))
 
 function findLast (word) {
-	for(var i = 0; i == word.length; i++) { 
+	for(var i = 0; i < word.length; i++) { 
 	}
-	return word[i];
+	return word[(i-1)];
 }
 
 console.log(findLast("ends"))
@@ -20,9 +20,9 @@ function findCube (number) {
 console.log(findCube(5))
 
 function reverse (unreversed) {
-	var output;
-	for(var i = unreversed.length; i > -1; i--) {
-		var output = output + unreversed[i];
+	var output = "";
+	for(var i = unreversed.length -1; i > -1; i--) {
+		output += unreversed[i];
 	}
 	return output;
 }
@@ -50,9 +50,15 @@ var Japan = {
 }
 
 function disorganiser (country) {
-	return Object.values(country)
-	return Object.keys(country)
-
+	NewObject = {
+		keys : [],
+		values : []
+	}
+	for (var key in country) {
+		NewObject.keys.push(key)	
+		NewObject.values.push(country[key]) 
+	}
+	return NewObject 
 }
 
 console.log(disorganiser(Japan))
